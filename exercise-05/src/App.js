76baf2ae955499017a74dca14effa4c5551431f1
leneehe <qy.lena@gmail.com>
2024-09-01 17:55:58 -0400

    input change variable example

--- exercise-05/src/App.js
+++ exercise-05/src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [age, setAge] = useState(1);
+  const [inputValue, setInputValue] = useState("");
 
-
-  const increaseAge = () => {
-    setAge(age + 1)
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setInputValue(event.target.value)
   }
 
   return (
     <div className="App">
-      {age}
-      <button onClick={increaseAge}>Increase Age</button>
+     <input type="text" onChange={handleInputChange}/>
+     {inputValue}
     </div>
   );
 }
