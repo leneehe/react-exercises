d59e382fea990ace3257817886216f69997588ef
leneehe <qy.lena@gmail.com>
2024-09-01 17:14:15 -0400

    Create plants app using lists and ternary operator

--- exercise-04/src/App.js
+++ exercise-04/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import {User} from './User'
+import {planets} from './planets';
+import {Planet} from './Planet';
 
 function App() {
-  const users = [{name:'Lena', age: 31}, {name:'Kate', age: 25}, {name:'Jake', age: 33}, {name:'Jessica', age: 45}, {name:'Dustin', age: 21}];
-
     return (
       <div className="App">
-        {users.map(({name, age}, key) => {
-          return <User key={key} name={name} age={age}/>
-          })}
+        {planets.map(({name, isGasPlanet}) => 
+          (isGasPlanet && <Planet name={name}/>)
+          )}
       </div>
     );
-
 }
 
 export default App;
