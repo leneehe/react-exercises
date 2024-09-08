d6e1b976c345c5bab01761ef65af7ed4ff622ac2
leneehe <qy.lena@gmail.com>
2024-09-08 16:42:11 -0400

    create basic cat fact generator app

---
+++ exercise-08/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  fetch("https://catfact.ninja/fact")
+.then((res) => res.json())
+.then((data) => {
+  console.log(data)
+})
+
+  return (
+    <div className="App">
+      <button>Generate Cat Fact</button>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
