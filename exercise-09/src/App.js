1f15b480e6d66056cd358bcf945f479254233cd9
leneehe <qy.lena@gmail.com>
2024-09-09 04:00:14 -0400

    Add navbar and 404 page

--- exercise-09/src/App.js
+++ exercise-09/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Home} from './pages/Home';
 import { Menu } from './pages/Menu';
 import { Contact } from './pages/Contact';
+import { Navbar } from './Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>Page not Found!</h1>} />
         </Routes>
       </Router>
     </div>
