adedd1d32e68dc30d46e8b472a145f310cbfbdd0
leneehe <qy.lena@gmail.com>
2024-09-09 00:00:59 -0400

    create excuser app using fetch

--- exercise-08/src/App.js
+++ exercise-08/src/App.js
@@ -3,22 +3,25 @@ import Axios from 'axios';
 import {useState, useEffect} from 'react';
 
 function App() {
-  const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
+  const [excuse, setExcuse] = useState('');
 
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      setPredictedAge(res.data)
+  const fetchExcuse = (category) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}/`).then((res) => {
+      setExcuse(res.data[0].excuse)
     })
   }
 
   return (
     <div className="App">
-      <input onChange={(event) => setName(event.target.value)} placeholder="ie. Lena..."/>
-      <button onClick={fetchData}>Predict Age</button>
-      <h1>Name: {predictedAge?.name}</h1>
-      <h1>Predicted Age: {predictedAge?.age}</h1>
-      <h1>Count: {predictedAge?.count}</h1>
+      <h1>Generate an Excuse</h1>
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <br/><br/>
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <br/><br/>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <p>
+        {excuse}
+      </p>
     </div>
   );
 }
