3a8c1034a5c9f1dc1d40ce8424a24b1e86eeecdd
leneehe <qy.lena@gmail.com>
2024-09-11 00:43:06 -0400

    use react query to fetch data

--- exercise-09/src/pages/Home.js
+++ exercise-09/src/pages/Home.js
@@ -1,19 +1,24 @@
 import { useContext } from 'react';
 import { AppContext } from '../App';
 import {useQuery} from '@tanstack/react-query';
-import {Axios} from 'axios';
+import Axios from 'axios';
 
 export const Home = () => {
     const {username} = useContext(AppContext);
 
-    const {data} = useQuery({queryKey:["cat"], queryFn:() => {
-        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-      }});
+    const {data} = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+          Axios.get('https://catfact.ninja/fact').then((res) =>
+            res.data,
+          ),
+      })
+      
     return (
         <>
             <h1>This is the HOME PAGE</h1>
             <p>The user is {username}</p>
-            {/* <p>{data?.fact}</p> */}
+            <p>{data?.fact}</p>
         </>
     )
 }
