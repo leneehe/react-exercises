197a9a78418847a278b9718a298d0853da0133db
leneehe <qy.lena@gmail.com>
2024-09-12 20:15:26 -0400

    create basic form app

---
+++ exercise-10/src/Form.js
@@ -0,0 +1,12 @@
+export const Form = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Full Name..." />
+            <input type="text" placeholder="Email..." />
+            <input type="text" placeholder="Age..." />
+            <input type="text" placeholder="Password..." />
+            <input type="text" placeholder="Confirm Password..." />
+            <input type="submit" />
+        </form>
+    )
+}
