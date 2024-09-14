d3e31f8a39767bd7a82870455ddf0fcd65881437
leneehe <qy.lena@gmail.com>
2024-09-14 01:02:57 -0400

    convert to custom hook for toggle

--- exercise-11/src/App.js
+++ exercise-11/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+import { useToggle } from './useToggle';
 
 function App() {
-  const [isVisible, setInvisible] = useState(false);
-
+  const [isVisible, setIsVisible] = useToggle(false)
+  const [isVisible2, toggle] = useToggle()
   return (
     <div className="App">
-      <button onClick={() => setInvisible((prev) => !prev)}>   
+      <button onClick={setIsVisible}>   
         {isVisible ? "Hide" : "Show"}
       </button>
       {isVisible && <h1>Hidden text</h1>}
+
+      <button onClick={toggle}>   
+        {isVisible2 ? "Hide" : "Show"}
+      </button>
+      {isVisible2 && <h1>Hidden text</h1>}
     </div>
   );
 }
