aad030d48eb88321bb0f1ab38cbde2ceae8cf7bf
leneehe <qy.lena@gmail.com>
2024-09-16 05:02:55 -0400

    use redux toolkit to update global state

--- exercise-14/src/pages/Login.tsx
+++ exercise-14/src/pages/Login.tsx
@@ -1,9 +1,21 @@
+import {useState} from 'react';
+import {login, logout} from '../store'
+import {useDispatch, useSelector} from 'react-redux'
+
 export const Login = () => {
+    const [newUsername, setNewUsername] = useState("");
+
+    const dispatch = useDispatch()
+    const username = useSelector((state:any) => state.user.value.username)
+
     return (
         <>
-        <h1>THIS IS THE LOGIN PAGE</h1>
-        <input /> 
-        <button>Submit Login</button>
+        <h1>{username}</h1>
+        <input onChange={(e) => {
+            setNewUsername(e.target.value)
+        }}/> 
+        <button onClick={() => dispatch(login({username: newUsername}))} >Submit Login</button>
+        <button onClick={() => dispatch(logout(null))}>Logout</button>
         </>
     )
 }
